dbf64e6c48d32938807d7c17e67be16a13698c99
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2021-10-18 14:55:50 +0300

    add pie

--- src/web/wwwroot/js/Chart.js
+++ src/web/wwwroot/js/Chart.js
@@ -1,4 +1,4 @@
-﻿function GeneraPieChart(sumList) {
+﻿function GeneraPieChartBalans(sumList) {
     am4core.useTheme(am4themes_animated);
 
     var chart = am4core.create("chartdiv", am4charts.PieChart3D);
@@ -13,4 +13,38 @@
     series.dataFields.value = "sumPercentage";
  
     series.dataFields.category = "nameCategory";    
+}
+
+function GeneraPieChartExpenditure(sumList) {
+    am4core.useTheme(am4themes_animated);
+
+    var chart = am4core.create("chartdiv1", am4charts.PieChart3D);
+    chart.hiddenState.properties.opacity = 0;
+
+    chart.legend = new am4charts.Legend();
+
+    chart.data = sumList;
+
+
+    var series = chart.series.push(new am4charts.PieSeries3D());
+    series.dataFields.value = "sumPercentage";
+
+    series.dataFields.category = "nameCategory";
+}
+
+function GeneraPieChartReceipt(sumList) {
+    am4core.useTheme(am4themes_animated);
+
+    var chart = am4core.create("chartdiv2", am4charts.PieChart3D);
+    chart.hiddenState.properties.opacity = 0;
+
+    chart.legend = new am4charts.Legend();
+
+    chart.data = sumList;
+
+
+    var series = chart.series.push(new am4charts.PieSeries3D());
+    series.dataFields.value = "sumPercentage";
+
+    series.dataFields.category = "nameCategory";
 }
